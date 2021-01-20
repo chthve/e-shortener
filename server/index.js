5c523ef8c3c8afd5201ae936cf187737bbcf55c0
Charles-Thibault <charles.thibault.verrier@gmail.com>
2021-01-20 11:38:28 +0100

    feat(heroku): deployement

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 const connectDB = require('./db/db');
 const router = require('./router');
 
@@ -12,6 +13,7 @@ connectDB();
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.join(__dirname, 'client/build')));
 app.use(router);
 
 app.listen(PORT, () => {
